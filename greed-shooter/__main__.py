3ac3d3703dd5c28b2a7b7d4025338451de1fc649
Nelson Georges <georson00@gmail.com>
2022-07-11 17:52:52 -0400

    Update __main__.py

--- greed-shooter/__main__.py
+++ greed-shooter/__main__.py
@@ -1,3 +1,12 @@
+import os
+import random
+
+from game.casting.actor import Actor
+from game.casting.artifact import Artifact
+from game.casting.cast import Cast
+
+from game.directing.director import Director
+
 
 from game.services.keyboard_service import KeyboardService
 from game.services.video_service import VideoService
